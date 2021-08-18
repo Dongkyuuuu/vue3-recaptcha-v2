30556f254383b4e47282cc486237f64b7eee81f3
Dongkyuuuu <desktop@kakao.com>
2021-08-18 13:49:09 +0900

    init playground

---
+++ playground/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>test!</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "App",
+  setup: () => {},
+});
+</script>
+
+<style></style>
