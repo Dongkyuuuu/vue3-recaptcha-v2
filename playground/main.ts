376beb416de76c36f3672db4d4f3edf58db69cf3
dongkyuuuu <desktop@kakao.com>
2023-09-13 01:21:48 +0900

    v2 init

--- playground/main.ts
+++
@@ -1,7 +0,0 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import recaptch from "./utils/recaptcha";
-
-const app = createApp(App);
-
-app.use(recaptch).mount("#app");
--- src/useApi.ts
+++
@@ -1,6 +0,0 @@
-import { inject } from "vue";
-import { recaptchaV2Key } from "./symbols";
-
-export function useRecaptcha() {
-  return inject(recaptchaV2Key)!;
-}
