04630c8c4232d1eedaed7f045af2c78fd6f6a0f0
dongkyuuuu <desktop@kakao.com>
2023-09-13 13:28:07 +0900

    fix: render widget after script loaded

--- src/components/Recaptcha.vue
+++ src/components/Recaptcha.vue
@@ -1,9 +1,6 @@
-<template>
-  <div ref="reCAPTCHARef" />
-</template>
-
 <script setup lang="ts">
 import { ref, onBeforeMount, onMounted } from "vue";
+import { RECAPTCHA_SCRIPT_ID } from "../constants";
 import { useInstall } from "./../composables";
 import { ReCaptchaError } from "./../utils";
 
@@ -13,6 +10,7 @@ const props = defineProps<{
   theme?: "light" | "dark";
   size?: "normal" | "compact";
   tabindex?: number;
+  language?: string;
 }>();
 const emit = defineEmits<{
   (e: "widgetId", value: string): void;
@@ -22,28 +20,40 @@ const emit = defineEmits<{
 }>();
 
 const handleRenderRecaptcha = () => {
-  if (!window.grecaptcha) throw new ReCaptchaError("reCAPTCHA is not defined");
+  if (!window.grecaptcha) throw new ReCaptchaError("reCAPTCHA is not loaded");
   if (!reCAPTCHARef.value) throw new ReCaptchaError("element is not defined");
 
-  const { siteKey } = options;
-  const { theme, size, tabindex } = props;
-  const widgetId = window.grecaptcha.render(reCAPTCHARef.value, {
-    siteKey,
-    theme,
-    size,
-    tabindex,
-    callback: (response: unknown) => emit("loadCallback", response),
-    "expired-callback": () => emit("expiredCallback"),
-    "error-callback": () => emit("errorCallback"),
-  });
+  window.grecaptcha.ready(() => {
+    try {
+      const { sitekey } = options;
+      const { theme, size, tabindex } = props;
+      const widgetId = window.grecaptcha!.render(reCAPTCHARef.value!, {
+        sitekey,
+        theme,
+        size,
+        tabindex,
+        callback: (response: unknown) => emit("loadCallback", response),
+        "expired-callback": () => emit("expiredCallback"),
+        "error-callback": () => emit("errorCallback"),
+      });
 
-  emit("widgetId", widgetId);
+      emit("widgetId", widgetId);
+    } catch (e) {
+      throw new ReCaptchaError((e as Error).message);
+    }
+  });
 };
 
 onBeforeMount(() => {
-  handleGenerateScript();
+  handleGenerateScript(props.language);
 });
 onMounted(() => {
-  handleRenderRecaptcha();
+  document.getElementById(RECAPTCHA_SCRIPT_ID)!.onload = () => {
+    handleRenderRecaptcha();
+  };
 });
 </script>
+
+<template>
+  <div ref="reCAPTCHARef" />
+</template>
